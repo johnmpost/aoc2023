4fd1e31100016ee945d6a2699acf12ffd4c5c291
john <johnmichaelpost@gmail.com>
2023-12-02 23:10:52 -0600

    remove unused fn

--- src/Utils.purs
+++ src/Utils.purs
@@ -1,21 +1,7 @@
 module Utils where
 
-import Prelude
-
-import Control.Monad.Error.Class (try)
-import Data.Either (either)
 import Data.Maybe (Maybe(..))
-import Effect (Effect)
-import Node.Encoding (Encoding)
-import Node.FS.Sync (readTextFile)
-import Node.Path (FilePath)
 import Partial.Unsafe (unsafeCrashWith)
-
-readTextFileSafe :: Encoding -> FilePath -> Effect (Maybe String)
-readTextFileSafe encoding =
-  readTextFile encoding
-    >>> try
-    >>> map (either (const Nothing) Just)
 
 unsafeFromJust :: forall a. Maybe a -> a
 unsafeFromJust maybeVal = case maybeVal of
