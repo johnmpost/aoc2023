71583818b3b0456f2e177da2a38657b02aba0e7d
john <johnmichaelpost@gmail.com>
2023-12-03 00:30:02 -0600

    start to solve part 2

--- src/Day1.purs
+++ src/Day1.purs
@@ -3,11 +3,14 @@ module Day1 where
 import Prelude
 
 import Data.Array (catMaybes)
-import Data.Array.NonEmpty (fromArray, head, last)
+import Data.Array.NonEmpty (fromArray, head, last, toArray)
 import Data.CodePoint.Unicode (decDigitToInt)
 import Data.Foldable (sum)
 import Data.Maybe (fromJust)
 import Data.String (Pattern(..), split, toCodePointArray)
+import Data.String.Regex (match)
+import Data.String.Regex.Flags (global)
+import Data.String.Regex.Unsafe (unsafeRegex)
 import Partial.Unsafe (unsafePartial)
 
 calibrationValueFromString :: String -> Int
@@ -18,6 +21,16 @@ calibrationValueFromString =
     >>> fromArray
     >>> unsafePartial fromJust
     >>> (\n -> head n * 10 + last n)
+
+calibration2 :: String -> Int
+calibration2 =
+  match (unsafeRegex "(one|two)" global)
+    >>> unsafePartial fromJust
+    >>> toArray
+    >>> catMaybes
+    >>> fromArray
+    >>> unsafePartial fromJust
+    >>> \s -> 0
 
 transform :: String -> String
 transform =
