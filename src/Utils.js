58ebb43efc3f189283d326e298a35646a4aff4fd
john <johnmichaelpost@gmail.com>
2023-12-07 15:32:11 -0600

    add todo

--- src/Utils.js
+++ src/Utils.js
@@ -4,4 +4,5 @@ export const matchAll = (regex) => (str) => {
     const re = new RegExp(regex, 'g');
     const matches = [...str.matchAll(re)];
     return matches.map(x => ({match: x[0], index: x.index}))
+    // todo: also return the matched group as a Maybe
 }
