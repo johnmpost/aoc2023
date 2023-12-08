c675aeb73f12c0b84fe18e6bd0ba1b32f99c1282
john <johnmichaelpost@gmail.com>
2023-12-07 23:45:04 -0600

    day 4 part 2 slow

--- src/Day4.purs
+++ src/Day4.purs
@@ -2,15 +2,19 @@ module Day4 where
 
 import Prelude
 
-import Data.Foldable (length, sum)
+import Data.Array (fromFoldable, (!!))
+import Data.Array as A
+import Data.Foldable (length)
 import Data.Int (pow)
-import Data.List (List, intersect)
+import Data.List (List(..), intersect, range, (:))
 import Data.List.NonEmpty (toList)
+import Data.Maybe (fromJust)
 import Data.String (Pattern(..), split)
 import Parsing (Parser)
 import Parsing.Combinators (many1Till, sepBy1)
 import Parsing.String (string)
 import Parsing.String.Basic (intDecimal, whiteSpace)
+import Partial.Unsafe (unsafePartial)
 import Utils (unsafeRunParser)
 
 type Scratchcard = { cardNo :: Int, winningNums :: List Int, haveNums :: List Int }
@@ -30,16 +34,42 @@ matchesToPoints :: Int -> Int
 matchesToPoints 0 = 0
 matchesToPoints n = 2 `pow` (n - 1)
 
+toNumMatches :: Scratchcard -> Int
+toNumMatches { winningNums, haveNums } = intersect winningNums haveNums # length
+
 scratchcardToPoints :: Scratchcard -> Int
 scratchcardToPoints { winningNums, haveNums } =
   intersect winningNums haveNums
     # length
     # matchesToPoints
 
+-- transform :: String -> String
+-- transform =
+--   (split (Pattern "\n"))
+--     >>> map (unsafeRunParser scratchcard)
+--     >>> map scratchcardToPoints
+--     >>> sum
+--     >>> show
+
+processPile :: Array Int -> List Int -> Int -> Int
+processPile matchesPerCard pile processedCount =
+  case pile of
+    (Nil) -> processedCount
+    (cardNo : remCards) ->
+      let
+        numMatches = matchesPerCard !! cardNo # unsafePartial fromJust
+        newInts = if numMatches == 0 then (Nil) else range (cardNo + 1) (cardNo + numMatches)
+        newPile = remCards <> newInts
+      in
+        processPile matchesPerCard newPile (processedCount + 1)
+
 transform :: String -> String
-transform =
-  (split (Pattern "\n"))
-    >>> map (unsafeRunParser scratchcard)
-    >>> map scratchcardToPoints
-    >>> sum
-    >>> show
+transform = do
+  cardsByIndex <-
+    (split (Pattern "\n"))
+      >>> map (unsafeRunParser scratchcard)
+      >>> map toNumMatches
+      >>> fromFoldable
+  let initialPile = range 0 (A.length cardsByIndex - 1)
+  let totalNumCards = processPile cardsByIndex initialPile 0
+  pure $ show totalNumCards
